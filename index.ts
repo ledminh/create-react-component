d6050305cc917dbd92e1bfd370f143aad5aa0e0e
Minh Le <duyminh.le310@gmail.com>
2022-12-07 16:30:39 -0500

    Change package.json

--- index.ts
+++ index.ts
@@ -1,36 +1,41 @@
-if(process.argv.length < 3)
-    throw Error('Component name not found');
-
-const pathPointerIndex = process.argv.indexOf('-p');
-
-let path:string|null = null;
-
-if(pathPointerIndex !== -1) {
-    path = process.argv[pathPointerIndex + 1];
-    
-    if(path[path.length - 1] !== '/')
-    path = path + '/';
-}
-
-const componentNames = process.argv.slice(2).filter(elem => elem !== '-p' && elem !== path?.slice(0, path.length - 1));
-
-
-
 import fs from 'fs';
 import getIndexText from './indexText';
 
 
-componentNames.forEach(componentName => {
-    const indexText = getIndexText(componentName);
+function main() {
 
-    fs.mkdir((path? path : '') + componentName, {recursive:true}, (err) => {
-        if(err) throw err;
+
+
+    if(process.argv.length < 3)
+        throw Error('Component name not found');
     
-        fs.writeFileSync((path? path : '') + componentName + '/index.tsx', indexText);
+    const pathPointerIndex = process.argv.indexOf('-p');
     
-        fs.writeFileSync((path? path : '') + componentName + `/${componentName}.module.scss`, '');
-    });
-})
+    let path:string|null = null;
+    
+    if(pathPointerIndex !== -1) {
+        path = process.argv[pathPointerIndex + 1];
+        
+        if(path[path.length - 1] !== '/')
+        path = path + '/';
+    }
+    
+    const componentNames = process.argv.slice(2).filter(elem => elem !== '-p' && elem !== path?.slice(0, path.length - 1));
+
+    
+    componentNames.forEach(componentName => {
+        const indexText = getIndexText(componentName);
+    
+        fs.mkdir((path? path : '') + componentName, {recursive:true}, (err) => {
+            if(err) throw err;
+        
+            fs.writeFileSync((path? path : '') + componentName + '/index.tsx', indexText);
+        
+            fs.writeFileSync((path? path : '') + componentName + `/${componentName}.module.scss`, '');
+        });
+    })
+}
 
 
 
+export default main;
